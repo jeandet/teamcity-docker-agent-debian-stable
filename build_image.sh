83305a170cb8f7b3d48d069dd5346457e9a08a76
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-27 19:13:53 +0100

    Added agent update on image build
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,6 @@
 docker build -t jeandet/teamcity_agent_debian_stable .
+docker run -d=true -e SERVER_URL=https://hephaistos.lpp.polytechnique.fr/teamcity --name=teamcity_agent_debian_stable -it jeandet/teamcity_agent_debian_stable &
+sleep 300
+docker stop teamcity_agent_debian_stable
+docker commit teamcity_agent_debian_stable jeandet/teamcity_agent_debian_stable
+docker rm teamcity_agent_debian_stable
